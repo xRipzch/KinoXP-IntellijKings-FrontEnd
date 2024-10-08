d48e153afe676ef6e732a5f4ce69be71177000fc
David Lu <davidlu1999@gmail.com>
2024-10-08 21:46:08 +0200

    Navbar styling - Moved css elements to their appropriate .css

---
+++ js/deleteshowing.js
@@ -0,0 +1,25 @@
+const inpField = document.getElementById("input-showing-id");
+const delButton = document.getElementById("delete-showing");
+
+///////////////////////////////////////////////////////////////////////
+
+async function deleteShowing (showingId) {
+    const url = "http://localhost:8080/showing/" + showingId ;
+    const confirmDelete = confirm(`Are you sure you want to delete "${showingId}"?`);
+
+    if (confirmDelete) {
+        const fetchOptions = {
+            method: "DELETE",
+        };
+
+        const response = await fetch(url, fetchOptions);
+
+        (response.ok) ? alert('Showing deleted successfully!') : alert('Error, could not delete showing');
+    }
+}
+
+delButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    deleteShowing(inpField.value)
+})
--- js/navbar.js
+++ js/navbar.js
@@ -5,14 +5,18 @@ class NavBar extends HTMLElement {
         link.href = '../CSS/navbar.css';
         document.head.appendChild(link);
         this.innerHTML =  ` 
-            <nav>               
-                <ul>
-                    <li><a href="../html/index.html">Home</a></li>
+            <nav>
+                <ul class="left-column">
                     <li><a href="../html/all-movies.html">All movies</a></li>
-                    <li><a href="../html/login.html">Log in</a></li>
                     <li><a href="../html/admin.html">Admin</a></li>
-                    <li><a href="../html/movie-details.html">Movie details</a></li>
                     <li><a href="../html/showings.html">Showings</a></li>
+                </ul>
+                <ul class="middle-column">
+                    <img src="../images/logo.png" alt="Logo">
+                    <!--<li><a href="../html/index.html">KinoXP</a></li>-->
+                </ul>
+                <ul class="right-column">
+                    <li><a href="../html/login.html">Login</a></li>
                 </ul>
             </nav>
         `; // adding html navbar to custom element.
